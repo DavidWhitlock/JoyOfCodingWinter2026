63da894b39a8ea8dd685ecb2ac7e95a1098a19ff
David Whitlock <david.m.whitlock@gmail.com>
2026-03-09 18:04:52 -0700

    Close the CalculatorActivity when the button is clicked.

--- phonebill-android/app/src/main/java/edu/pdx/cs/joy/whitlock/CalculatorActivity.java
+++ phonebill-android/app/src/main/java/edu/pdx/cs/joy/whitlock/CalculatorActivity.java
@@ -1,6 +1,7 @@
 package edu.pdx.cs.joy.whitlock;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,9 @@ public class CalculatorActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void goBackToMainActivity(View view) {
+        finish();
     }
 }
