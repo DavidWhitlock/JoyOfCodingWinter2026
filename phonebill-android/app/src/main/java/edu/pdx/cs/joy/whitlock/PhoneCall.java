df67108ee58c47bd14f0e7309783657f360f3120
David Whitlock <david.m.whitlock@gmail.com>
2026-03-09 19:01:55 -0700

    Add a dependency on my jar file with the Abstract classed in it.

---
+++ phonebill-android/app/src/main/java/edu/pdx/cs/joy/whitlock/PhoneCall.java
@@ -0,0 +1,25 @@
+package edu.pdx.cs.joy.whitlock;
+
+import edu.pdx.cs.joy.AbstractPhoneCall;
+
+public class PhoneCall extends AbstractPhoneCall {
+    @Override
+    public String getCaller() {
+        return "";
+    }
+
+    @Override
+    public String getCallee() {
+        return "";
+    }
+
+    @Override
+    public String getBeginTimeString() {
+        return "";
+    }
+
+    @Override
+    public String getEndTimeString() {
+        return "";
+    }
+}
